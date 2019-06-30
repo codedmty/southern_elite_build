d797e9d76f773efcee23e3475be442cf57276d80
eddiefrs <eddie@flexrentalsolutions.com>
2019-06-29 19:18:29 -0500

    First working version of the Southern Elite App, BETA

---
+++ precache-manifest.5135c46ace2cda4fefbb8aff7cb6bb02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cda7ce130d2bc6ac407234570428d3f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b01a93ebd9199494411",
+    "url": "/static/css/main.068aa7d3.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "4b01a93ebd9199494411",
+    "url": "/static/js/main.f77abb45.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "729500e53b5622098cc489dd3010588b",
+    "url": "/static/media/logo.729500e5.png"
+  }
+]);
